0d20a4ab196744e7e6729ac3395449836ec4cd7e
Rick708 <Rick708@users.noreply.github.com>
2019-12-19 14:40:39 +0900

    fin

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -23,12 +23,12 @@ class LinebotController < ApplicationController
         when Line::Bot::Event::MessageType::Text
           # event.message['text']：ユーザーから送られたメッセージ
           input = event.message['text']
-          url  = "https://www.drk7.jp/weather/xml/13.xml"
+          url  = "https://www.drk7.jp/weather/xml/27.xml"
           xml  = open( url ).read.toutf8
           doc = REXML::Document.new(xml)
-          xpath = 'weatherforecast/pref/area[4]/'
+          xpath = 'weatherforecast/pref/area[1]/'
           # 当日朝のメッセージの送信の下限値は20％としているが、明日・明後日雨が降るかどうかの下限値は30％としている
-          min_per = 30
+          min_per = 50
           case input
             # 「明日」or「あした」というワードが含まれる場合
           when /.*(明日|あした).*/
@@ -38,10 +38,10 @@ class LinebotController < ApplicationController
             per18to24 = doc.elements[xpath + 'info[2]/rainfallchance/period[4]'].text
             if per06to12.to_i >= min_per || per12to18.to_i >= min_per || per18to24.to_i >= min_per
               push =
-                "明日の天気だよね。\n明日は雨が降りそうだよ(>_<)\n今のところ降水確率はこんな感じだよ。\n　  6〜12時　#{per06to12}％\n　12〜18時　 #{per12to18}％\n　18〜24時　#{per18to24}％\nまた明日の朝の最新の天気予報で雨が降りそうだったら教えるね！"
+                "明日の天気\n明日は雨が降るかも？\n今のところ降水確率はこんな感じだよ。\n　  6〜12時　#{per06to12}％\n　12〜18時　 #{per12to18}％\n　18〜24時　#{per18to24}％\nまた明日の朝の最新の天気予報で雨が降りそうだったら教えるね！"
             else
               push =
-                "明日の天気？\n明日は雨が降らない予定だよ(^^)\nまた明日の朝の最新の天気予報で雨が降りそうだったら教えるね！"
+                "明日の天気\n明日は雨が降らないかも？\nまた明日の朝の最新の天気予報で雨が降りそうだったら教えるね！"
             end
           when /.*(明後日|あさって).*/
             per06to12 = doc.elements[xpath + 'info[3]/rainfallchance/period[2]l'].text
@@ -49,17 +49,17 @@ class LinebotController < ApplicationController
             per18to24 = doc.elements[xpath + 'info[3]/rainfallchance/period[4]l'].text
             if per06to12.to_i >= min_per || per12to18.to_i >= min_per || per18to24.to_i >= min_per
               push =
-                "明後日の天気だよね。\n何かあるのかな？\n明後日は雨が降りそう…\n当日の朝に雨が降りそうだったら教えるからね！"
+                "明後日の天気\n明後日は雨が降るかもしれまへんわ！\n当日の朝に雨が降りそうだったら教えますわ！"
             else
               push =
-                "明後日の天気？\n気が早いねー！何かあるのかな。\n明後日は雨は降らない予定だよ(^^)\nまた当日の朝の最新の天気予報で雨が降りそうだったら教えるからね！"
+                "明後日の天気\n明後日は雨は降らない予定でっせ\n当日の朝の最新の天気予報で雨が降りそうやったわ教えますわ！"
             end
           when /.*(かわいい|可愛い|カワイイ|きれい|綺麗|キレイ|素敵|ステキ|すてき|面白い|おもしろい|ありがと|すごい|スゴイ|スゴい|好き|頑張|がんば|ガンバ).*/
             push =
-              "ありがとう！！！\n優しい言葉をかけてくれるあなたはとても素敵です(^^)"
+              "ありがとう！！！\n優しい言葉をかけてくれるあなたはとても素敵です。でへへ〜(๑╹ω╹๑ )"
           when /.*(こんにちは|こんばんは|初めまして|はじめまして|おはよう).*/
             push =
-              "こんにちは。\n声をかけてくれてありがとう\n今日があなたにとっていい日になりますように(^^)"
+              "こんにちは。\n声をかけてくれてありがとう\n今日があなたにとっていい日になりますように(☝︎ ՞ਊ ՞)☝︎"
           else
             per06to12 = doc.elements[xpath + 'info/rainfallchance/period[2]l'].text
             per12to18 = doc.elements[xpath + 'info/rainfallchance/period[3]l'].text
@@ -83,7 +83,7 @@ class LinebotController < ApplicationController
           end
           # テキスト以外（画像等）のメッセージが送られた場合
         else
-          push = "テキスト以外はわからないよ〜(；；)"
+          push = "なんて言ってるかわからないよ〜( ；∀；)"
         end
         message = {
           type: 'text',
--- lib/tasks/scheduler.rake
+++ lib/tasks/scheduler.rake
@@ -13,25 +13,25 @@ task :update_feed => :environment do
   }
 
   # 使用したxmlデータ（毎日朝6時更新）：以下URLを入力すれば見ることができます。
-  url  = "https://www.drk7.jp/weather/xml/13.xml"
+  url  = "https://www.drk7.jp/weather/xml/27.xml"
   # xmlデータをパース（利用しやすいように整形）
   xml  = open( url ).read.toutf8
   doc = REXML::Document.new(xml)
   # パスの共通部分を変数化（area[4]は「東京地方」を指定している）
-  xpath = 'weatherforecast/pref/area[4]/info/rainfallchance/'
+  xpath = 'weatherforecast/pref/area[1]/info/rainfallchance/'
   # 6時〜12時の降水確率（以下同様）
   per06to12 = doc.elements[xpath + 'period[2]'].text
   per12to18 = doc.elements[xpath + 'period[3]'].text
   per18to24 = doc.elements[xpath + 'period[4]'].text
   # メッセージを発信する降水確率の下限値の設定
-  min_per = 20
+  min_per = 40
   if per06to12.to_i >= min_per || per12to18.to_i >= min_per || per18to24.to_i >= min_per
     word1 =
-      ["いい朝だね！",
-       "今日もよく眠れた？",
+      ["Good Morning！",
+       "よく眠れた？",
        "二日酔い大丈夫？",
-       "早起きしてえらいね！",
-       "いつもより起きるのちょっと遅いんじゃない？"].sample
+       "ゴミの日じゃない？大丈夫？",
+       "朝ごはんなににする？"].sample
     word2 =
       ["気をつけて行ってきてね(^^)",
        "良い一日を過ごしてね(^^)",
@@ -39,7 +39,7 @@ task :update_feed => :environment do
        "今日も一日楽しんでいこうね(^^)",
        "楽しいことがありますように(^^)"].sample
     # 降水確率によってメッセージを変更する閾値の設定
-    mid_per = 50
+    mid_per = 70
     if per06to12.to_i >= mid_per || per12to18.to_i >= mid_per || per18to24.to_i >= mid_per
       word3 = "今日は雨が降りそうだから傘を忘れないでね！"
     else
